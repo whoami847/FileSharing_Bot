a5167077db5cafcecdd812ef7e547ddfb7ac3838
whoami847 <mrbeastbangla069@gmail.com>
2025-04-04 11:07:55 +0600

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = list(map(int, os.getenv("ADMINS").split()))
